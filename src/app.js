b14bd93f46284586556e3ad2f80e40d202ab656d
Nicolas <nicolasjaiquel@gmail.com>
2023-02-11 08:11:56 -0500

    idk

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import employees from "./routes/employees.routes.js";
+import index from "./routes/index.routes.js";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT,
+} from "./config.js";
+const app = express();
+app.use(express.json());
+app.use("/api", employees);
+app.get("/hi", (req, res) => {
+  res.send("It's everything ok");
+});
+app.use(index);
+console.log(DB_DATABASE);
+console.log(DB_HOST);
+console.log(DB_PASSWORD);
+console.log(DB_USER);
+console.log(DB_PORT);
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-import express from "express";
-import employees from "./routes/employees.routes.js";
-import index from "./routes/index.routes.js";
-import {
-  PORT,
-  DB_DATABASE,
-  DB_HOST,
-  DB_PASSWORD,
-  DB_USER,
-  DB_PORT,
-} from "./config.js";
-const app = express();
-app.use(express.json());
-app.use("/api", employees);
-app.get("/hi", (req, res) => {
-  res.send("It's everything ok");
-});
-app.use(index);
-console.log(DB_DATABASE);
-console.log(DB_HOST);
-console.log(DB_PASSWORD);
-console.log(DB_USER);
-console.log(DB_PORT);
+import app from "./app.js";
+import { PORT } from "./config.js";
 console.log(`I am listening at port ${PORT}`);
 app.listen(PORT);
